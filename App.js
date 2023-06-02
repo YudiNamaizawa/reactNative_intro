8e1900ff19a344f1de78480d281d953f02471e04
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-06-01 22:50:10 -0300

    aula 01-06

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native'; 
+
+import Constants from 'expo-constants';
 
 import Apostila_003 from './components/apostila_003';
 import Apostila_004 from './components/apostila_004';
+
+const corApp = '#fff';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Apostila_004 />
-      <StatusBar style="auto" />
+      <StatusBar style={corApp==='#000' ? 'light' : 'dark'} />
     </View>
   );
 }
@@ -18,8 +22,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center', 
     alignItems: 'center', 
-    //paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#FFF',
-    padding: 8,
+    backgroundColor: corApp,
+    padding: 8, 
+    paddingTop: Constants.statusBarHeight,
   },
 });
--- components/apostila_004/index.js
+++ components/apostila_004/index.js
@@ -1,11 +1,35 @@
-import {View, Text} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 
 import styles from './styles';
 
 function Index() {
+
+    const [valorEntrada, setValorEntrada] = useState(''); 
+    const [mensagem, setMensagem] = useState('Mensagem');
+
     return(
         <View style={styles.container}>
             <Text style={styles.texto}>Apostila 4</Text>
+
+            <Text style={styles.txtSaida}>{mensagem}</Text>
+
+            <TextInput 
+                style={styles.txtEntrada} 
+                onChangeText={(saida) => setValorEntrada(saida)} 
+                value={valorEntrada}
+            />
+
+            <TouchableOpacity 
+                style={styles.button} 
+                onPress={() => {
+                    setMensagem(valorEntrada); 
+                    setValorEntrada('');
+                }}
+            >
+                <Text style={styles.textButton}>Exibir mensagem</Text>
+            </TouchableOpacity>
+
         </View>
     );
 }
