4760280c4f7c15b0ff61cdddb9bbd18870d8be1a
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-08-15 22:50:51 -0300

    intro navegacao drawer

--- App.js
+++ App.js
@@ -2,23 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native'; 
 
 import Constants from 'expo-constants';
-
-import Exemplo_001 from './components/exemplo_001';
-import Exemplo_002 from './components/exemplo_002';
-import Exemplo_003 from './components/exemplo_003';
-import Exemplo_004 from './components/exemplo_004'; 
-import Exemplo_005 from './components/exemplo_005'; 
-import Exemplo_006 from './components/exemplo_006'; 
-import Exemplo_007 from './components/exemplo_007'; 
-import Exemplo_008 from './components/exemplo_008';
-
-import Atividade_001 from './components/atividade_001'; 
-import Atividade_002 from './components/atividade_002'; 
-import Atividade_003 from './components/atividade_003'; 
-import Atividade_004 from './components/atividade_004'; 
-import Atividade_005 from './components/atividade_005'; 
-import Atividade_006 from './components/atividade_006'; 
-import Atividade_007 from './components/atividade_007'; 
+import 'react-native-gesture-handler';
 
 import Velha from './components/velha';
 
@@ -27,7 +11,7 @@ const corApp = '#fff';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo_008 />
+      <Velha />
       <StatusBar style={corApp==='#000' ? 'light' : 'dark'} />
     </View>
   );
